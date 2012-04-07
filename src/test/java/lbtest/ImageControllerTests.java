38c9093138f8081188e0602452e3da2ec1942a15
Gavin Gray <gavin@vmware.com>
2012-04-07 15:05:08 -0400

    Added RestTemplate to tests.

--- src/test/java/lbtest/ImageControllerTests.java
+++ src/test/java/lbtest/ImageControllerTests.java
@@ -1,16 +1,21 @@
 package lbtest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.web.client.RestTemplate;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import static org.testng.Assert.fail;
 
 @Test
 @ContextConfiguration(value = "classpath:test-context.xml")
 public class ImageControllerTests extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private RestTemplate template;
+
     @Test
-    void canUploadImageToDatabase() {
-        fail("fail");
+    void canUploadImage() {
+        Assert.assertNotNull(template);
     }
 }
