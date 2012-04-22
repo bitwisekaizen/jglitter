140861f34a36446f76affa43edb5c110bc9a4fdb
Gavin Gray <gavin@vmware.com>
2012-04-22 16:54:09 -0400

    Moved jquery stuff out of jsp. Got refresh of select list working.

--- src/main/java/harvard/marshallable/Images.java
+++ src/main/java/harvard/marshallable/Images.java
@@ -1,5 +1,6 @@
 package harvard.marshallable;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +19,7 @@ public class Images {
         }
     }
 
+    @XmlElement(name = "image")
     public List<Image> getImages() {
         return images;
     }
